bbfc4a8cad385e4b3f073a32181959a48a95d88c
sthobis <s.thobi.sinaga@gmail.com>
2019-02-22 22:34:21 +0700

    chore: setup next project

---
+++ components/Menu.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+const Menu = () => {
+  return (
+    <div className="menu nes-container is-rounded">
+      <h1 className="title">Guess My Drawing!</h1>
+      <button type="button" className=" option nes-btn is-primary">
+        Create Room
+      </button>
+      <Link href="/lobby">
+        <a className="option nes-btn is-primary">Join Room</a>
+      </Link>
+      <style jsx>{`
+        .menu {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          margin: 50px;
+          padding: 50px;
+        }
+
+        .title {
+          margin: 0 0 50px 0;
+        }
+
+        .option {
+          margin: 0 0 20px 0;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Menu;
